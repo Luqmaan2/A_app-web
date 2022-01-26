2639c1e359c0b304bcf3895f81fac072a8d33b0e
Luqmaan2 <73324519+Luqmaan2@users.noreply.github.com>
2022-01-26 16:00:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.css';
+
+export default function App() {
+  //A_L
+  return (
+  <div>
+    <center>
+    <div className='Logintext'>Login</div>
+    <img src="https://raw.githubusercontent.com/Luqmaan2/logo/Pages/favicon.ico" alt='Logo' className='Logo'  />
+    
+    </center>
+    
+    <input className='passwod'placeholder='password' type='password' />
+  <button className='Loginbtn' type='submit' >Login</button>
+  </div>
+  );
+}
+
